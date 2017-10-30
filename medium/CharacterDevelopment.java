df68362421daac0004b136dce31c170b30373262
Krzyminski.Bartlomiej <krzyminski.bartlomiej@gmail.com>
2017-10-30 22:39:21 +0100

    Solved medium level problem CharacterDevelopment

--- medium/CharacterDevelopment.java
+++ medium/CharacterDevelopment.java
@@ -1,5 +1,6 @@
 package medium;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -28,6 +29,53 @@ public class CharacterDevelopment {
 		Scanner scanInput = new Scanner(System.in);
 		Integer howManyTimes = scanInput.nextInt();
 		scanInput.close();
+		BigInteger summary = BigInteger.ZERO;
+
+		/**
+		 * To find how many possible relations can be between each point and
+		 * subset, pattern for combinations without repetitions can be used. In
+		 * that case i value stands for number of elements in subset from
+		 * howManyTimes elements set. Each value returned from pattern is added
+		 * to summary which stores number of possible relations.
+		 * 
+		 */
+
+		for (Integer i = 2; i <= howManyTimes; i++) {
+			summary = summary.add(combinationWithoutReps(BigInteger.valueOf(i.intValue()),
+					BigInteger.valueOf(howManyTimes.intValue())));
+		}
+		System.out.println(summary);
 	}
 
+	/**
+	 * Method that uses combinations without repetitions pattern.
+	 * 
+	 * @param k
+	 *            length of subset
+	 * @param n
+	 *            length of main set
+	 * @return number of combinations without repetitions
+	 * 
+	 */
+
+	static BigInteger combinationWithoutReps(BigInteger k, BigInteger n) {
+		return strong(n).divide((strong(k).multiply(strong(n.subtract(k)))));
+	}
+
+	/**
+	 * Recursion used to create strong value which means X!
+	 * 
+	 * @param strongValue
+	 *            value to be !
+	 * @return while the strongValue isn't less than one is multiplied by self -
+	 *         1
+	 *         
+	 */
+
+	static BigInteger strong(BigInteger strongValue) {
+		if (strongValue.compareTo(BigInteger.ONE) == -1)
+			return BigInteger.ONE;
+		else
+			return strongValue.multiply(strong(strongValue.subtract(BigInteger.ONE)));
+	}
 }
